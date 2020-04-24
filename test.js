500355996953391b7bdda2a26a73512756b53a0f
VolodymyrHav <dem82003@protonmail.com>
2020-04-24 11:03:00 +0300

    adding functionality for working with 'any' keyword

--- test.js
+++ test.js
@@ -4,8 +4,16 @@ function attempt(available, allowed, preffered ) {
     let availableSet = new Set(available)
     let allowedSet = new Set(allowed)
     let prefferedSet = new Set(preffered)
+
+    let allowedHasAny = allowed.includes('any')
+    let prefferedHasAny = preffered.includes('any')
     
-    let intersection = new Set([...availableSet].filter(x => allowedSet.has(x)));
+    let intersection
+    if (allowedHasAny) {
+        intersection = new Set(available)
+    } else {
+        intersection = new Set([...availableSet].filter(x => allowedSet.has(x)));
+    }
 
     if (intersection.size == 0) {
         return []
@@ -13,49 +21,51 @@ function attempt(available, allowed, preffered ) {
 
     let resultSet = new Set()
 
-    for (let value of prefferedSet) {
-        if (intersection.has(value)) {
-            resultSet.add(value)
-            continue
-        } 
-
-        let found = false
-        for (let elem of intersection) {
-            
-            if (elem > value) {
-                resultSet.add(elem)
-                found = true
-                break
+    if (prefferedHasAny) {
+        intersection.forEach(resultSet.add, resultSet) 
+    } else {
+        for (let value of prefferedSet) {
+            if (intersection.has(value)) {
+                resultSet.add(value)
+                continue
+            } 
+    
+            let found = false
+            for (let elem of intersection) {
+                
+                if (elem > value) {
+                    resultSet.add(elem)
+                    found = true
+                    break
+                }
             }
-        }
-
-        if (found) {
-            continue
-        }
-
-        let lastValue
-        let temp = []
-        for (let elem of intersection) {
-            if (elem < value) {
-                temp.push(elem)
+    
+            if (found) {
+                continue
             }
-        }
-        lastValue = temp.pop()
-        resultSet.add(lastValue)
-
-        for (let elem of resultSet) {
-            
-            if (elem != value) {
-                resultSet.add(elem)
-                found = true
-                break
+    
+            let lastValue
+            let temp = []
+            for (let elem of intersection) {
+                if (elem < value) {
+                    temp.push(elem)
+                }
+            }
+            lastValue = temp.pop()
+            resultSet.add(lastValue)
+    
+            for (let elem of resultSet) {
+                
+                if (elem != value) {
+                    resultSet.add(elem)
+                    found = true
+                    break
+                }
             }
         }
     }
-    
-    return Array.from(resultSet)
 
-    
+    return Array.from(resultSet)
 }
 
 function testAttempt() {
@@ -68,10 +78,11 @@ function testAttempt() {
     assert.deepEqual(attempt([720], [240, 360, 1080], [240, 360]), [])
     assert.deepEqual(attempt([240, 360], [240, 360], [720, 1080]), [360])
     
+    assert.deepEqual(attempt([240, 360, 720], [360, 'any'], [360, 720]), [360, 720])
+    assert.deepEqual(attempt([240, 360, 720], [240, 360, 720], ['any', 720]), [240, 360, 720])
+    assert.deepEqual(attempt([240, 360, 720], [360, 1080], ['any', 720]), [360])
+    assert.deepEqual(attempt([240, 360, 720], [1080], ['any', 720]), [])
     
-    
-
-
     console.log('Success') 
 }
 
